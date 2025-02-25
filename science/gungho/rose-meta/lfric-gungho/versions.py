d7853f9eccda0f63a8aa404e49c242742dde4ac6
oakleybrunt <enquiries@metoffice.gov.uk>
2025-02-25 08:56:41 +0000

    '''Further Commit''' ticket:84: Chris Smith : Vertical profile of horizontal wind forcing
    git-svn-id: https://metomi/svn/lfric_apps.xm/main/trunk@7799 3896877f-8c05-4358-bc88-7947bb4308c7

--- science/gungho/rose-meta/lfric-gungho/versions.py
+++ science/gungho/rose-meta/lfric-gungho/versions.py
@@ -17,6 +17,58 @@ class UpgradeError(Exception):
     __str__ = __repr__
 
 
+class vn20_t85(MacroUpgrade):
+   """Upgrade macro for ticket #85 by Chris Smith."""
+
+   BEFORE_TAG = "vn2.0"
+   AFTER_TAG = "vn2.0_t85"
+
+   def upgrade(self, config, meta_config=None):
+       # Commands From: science/gungho/rose-meta/lfric-gungho
+       """Add new geostrophic_forcing namelist"""
+       source = self.get_setting_value(
+           config, ["file:configuration.nml", "source"]
+       )
+       source = re.sub(
+           r"namelist:formulation",
+           r"namelist:formulation" + "\n" + " (namelist:geostrophic_forcing)",
+           source,
+       )
+       self.change_setting_value(
+           config, ["file:configuration.nml", "source"], source
+       )
+       """Add geostrophic_forcing setting to external_forcing namelist"""
+       self.add_setting(
+           config,
+           ["namelist:external_forcing", "geostrophic_forcing"],
+           ".false.",
+       )
+       """Add default data for geostrophic_forcing namelist"""
+       self.add_setting(config, ["namelist:geostrophic_forcing"])
+       self.add_setting(
+           config, ["namelist:geostrophic_forcing", "coordinate"], "'height'"
+       )
+       self.add_setting(
+           config, ["namelist:geostrophic_forcing", "heights"], "0.0"
+       )
+       self.add_setting(
+           config, ["namelist:geostrophic_forcing", "number_heights"], "1"
+       )
+       self.add_setting(
+           config, ["namelist:geostrophic_forcing", "number_times"], "1"
+       )
+       self.add_setting(
+           config, ["namelist:geostrophic_forcing", "profile_data_u"], "0.0"
+       )
+       self.add_setting(
+           config, ["namelist:geostrophic_forcing", "profile_data_v"], "0.0"
+       )
+       self.add_setting(
+           config, ["namelist:geostrophic_forcing", "times"], "0.0"
+       )
+       return config, self.reports
+
+
 class vn20_t358(MacroUpgrade):
     """Upgrade macro for ticket #358 by Joshua Dendy."""
 
